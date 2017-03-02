bfd9fe0f78e12131ba0aa1a3b0499e0eec9eedbf
Shun Yu Rao <shunyu.rao@gmail.com>
2017-03-02 12:03:34 -0500

    Base webhook setup

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# Simple Hello World Node.js sample for Google App Engine
+# Anonymous Facebook Chatbot
+Chat with strangers
 
-This sample demonstrates a tiny Hello World Node.js app for [Google App Engine Flexible Environment](https://cloud.google.com/appengine).
+## Garbanzo beans
+High in protein
 
-## Running locally
-
-Refer to the [appengine/README.md](../README.md) file for instructions on
-running and deploying.
--- app.js
+++ app.js
@@ -1,16 +1,61 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!');
+const conf = require('./conf');
+
+app.set('port', (process.env.PORT || conf.PORT));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('w e w l a d');
+})
+
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === conf.VERIFY_TOKEN) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Invalid token');
+  }
 });
 
-// Start the server
-const PORT = process.env.PORT || 420;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+app.post('/webhook', function (req, res) {
+    let events = req.body.entry[0].messaging;
+    for (let i = 0; i < events.length; i++) {
+        let event = events[i];
+        let sender = event.sender.id;
+        if (event.message && event.message.text) {
+            let text = event.message.text;
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+        }
+    }
+    console.log(events);
+    res.sendStatus(200);
 });
-// [END app]
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: conf.FB_ENDPOINT_URL,
+        qs: {access_token:conf.PAGE_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+app.listen(app.get('port'), function() {
+  console.log('running on port', app.get('port'));
+});
