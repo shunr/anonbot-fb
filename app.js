1400713947f7454a6ccf696230c06c3a4ab8d533
Shun Yu Rao <shunyu.rao@gmail.com>
2017-03-02 17:23:47 -0500

    working replies

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 'use strict'
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
 
 const conf = require('./conf');
 
 app.set('port', (process.env.PORT || conf.PORT));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
+https.createServer({
+  key: fs.readFileSync(conf.SSL_CONFIG.KEY),
+  cert: fs.readFileSync(conf.SSL_CONFIG.CERT),
+  ca: fs.readFileSync(conf.SSL_CONFIG.CA)
+}, app).listen(app.get('port'), function() {
+  console.log('running on port', app.get('port'));
+});
+
+app.get('/', function(req, res) {
   res.send('w e w l a d');
 })
 
-app.get('/webhook', function (req, res) {
+app.get('/webhook', function(req, res) {
   if (req.query['hub.verify_token'] === conf.VERIFY_TOKEN) {
     res.send(req.query['hub.challenge']);
   } else {
@@ -23,39 +34,42 @@ app.get('/webhook', function (req, res) {
   }
 });
 
-app.post('/webhook', function (req, res) {
-    let events = req.body.entry[0].messaging;
-    for (let i = 0; i < events.length; i++) {
-        let event = events[i];
-        let sender = event.sender.id;
-        if (event.message && event.message.text) {
-            let text = event.message.text;
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
-        }
+app.post('/webhook', function(req, res) {
+  let events = req.body.entry[0].messaging;
+  for (let i = 0; i < events.length; i++) {
+    let event = events[i];
+    let sender = event.sender.id;
+    if (event.message && event.message.text) {
+      let text = event.message.text;
+      sendTextMessage(sender, text);
     }
-    console.log(events);
-    res.sendStatus(200);
+  }
+  console.log(events);
+  res.sendStatus(200);
 });
+
 
 function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: conf.FB_ENDPOINT_URL,
-        qs: {access_token:conf.PAGE_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+  let messageData = {
+    text: text
+  }
+  request({
+    url: conf.FB_ENDPOINT_URL,
+    qs: {
+      access_token: conf.PAGE_TOKEN
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: sender
+      },
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
 }
-
-app.listen(app.get('port'), function() {
-  console.log('running on port', app.get('port'));
-});
