0d151cac3729ad77c2e8fcf05ce56db30349efea
Shun Yu Rao <shunyu.rao@gmail.com>
2017-03-02 22:58:57 -0500

    update sample config

--- conf.sample.js
+++ conf.sample.js
@@ -5,21 +5,9 @@ var conf = module.exports = {
   FB_SETTINGS_URL: 'https://graph.facebook.com/v2.6/me/thread_settings',
   PORT: 443,
   SSL_CONFIG: {
-    KEY: "/path/to/privkey1.pem",
-    CERT: "/path/to/fullchain1.pem",
-    CA: "/path/to/chain1.pem"
+    KEY: "/etc/letsencrypt/archive/anonbot.gq/privkey1.pem",
+    CERT: "/etc/letsencrypt/archive/anonbot.gq/fullchain1.pem",
+    CA: "/etc/letsencrypt/archive/anonbot.gq/chain1.pem"
   },
-  PERSISTENT_MENU: {
-    "setting_type": "call_to_actions",
-    "thread_state": "existing_thread",
-    "call_to_actions": [{
-      "type": "postback",
-      "title": "Chat with another user",
-      "payload": "AB_REROLL_CHAT_PARTNER"
-    }, {
-      "type": "postback",
-      "title": "Stop chatting",
-      "payload": "AB_STOP_CHAT"
-    }]
-  }
+  DB_PATH: "/home/anonbotdb"
 };
